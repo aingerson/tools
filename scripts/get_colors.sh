cf9cf6cfa4ed5b3edb85dc4f66bcd5c34beb7eb5
Alexia Ingerson <alexia.ingerson@intel.com>
2021-09-17 10:43:17 -0700

    fix color instal if .vim directory already exists
    Signed-off-by: Alexia Ingerson <alexia.ingerson@intel.com>

--- scripts/get_colors.sh
+++ scripts/get_colors.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mkdir ~/.vim
-git clone https://github.com/flazz/vim-colorschemes.git ~/.vim
+mkdir -p ~/.vim/colors
+git clone https://github.com/flazz/vim-colorschemes.git
+cp ./vim-colorschemes/colors/* ~/.vim/colors
+rm -rf ./vim-colorschemes
