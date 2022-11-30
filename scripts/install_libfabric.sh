aa935ee13ecd178bdd1712e0fea85e9364633f1f
Alexia Ingerson <alexia.ingerson@intel.com>
2022-11-30 09:04:50 -0800

    scripts: add libfabric/fabtests build script
    Signed-off-by: Alexia Ingerson <alexia.ingerson@intel.com>

---
+++ scripts/install_libfabric.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+##SET YOUR VARS HERE##
+target=$HOME/install            #where to install to (make sure this is a dedicated directory because it will get force removed!)
+repo=$HOME/workspace/libfabric  #where the source code is
+debug=1                         #set to 1 for debug build
+build_fabtests=1                #set to 1 to build fabtests
+
+declare -a libfabric_args=(
+                           "--disable-usnic"
+                           "--disable-efa"
+                           "--disable-opx"
+                           "--disable-psm"
+                           "--disable-psm2"
+                           "--disable-psm3"
+                           )
+
+declare -a fabtests_args=(
+                         )
+##END SET VARS##
+
+
+
+
+
+
+if [ $debug -eq 1 ]; then
+        export CFLAGS="-g -O0"
+fi
+
+cd $repo
+
+##clean previous build
+rm -rf $target
+if [ $build_fbatests -eq 1 ]; then
+        (cd fabtests; make distclean)
+fi
+make distclean
+
+
+##build libfabric
+config_cmd="./configure --prefix=$target"
+for arg in "${libfabric_args[@]}"; do
+        config_cmd+=" $arg"
+done
+if [ $debug -eq 1 ]; then
+        config_cmd+=" --enable-debug"
+fi
+
+./autogen.sh
+$config_cmd
+make -j install
+
+
+if [ $build_fabtests -ne 1 ]; then
+        exit 0
+fi
+
+##build fabtests
+cd fabtests
+config_cmd="./configure --prefix=$target --with-libfabric=$target"
+for arg in "${fabtests_args[@]}"; do
+        config_cmd+=" $arg"
+done
+if [ $debug -eq 1 ]; then
+        config_cmd+=" --enable-debug"
+fi
+./autogen.sh
+./configure $fabtests_args
+make -j install
