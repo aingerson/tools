d429eecc4c7b8c6fd398da20b8e17088e597433d
alexia.ingerson <aingerso@jfcst-cb.cluster>
2023-07-11 14:14:12 -0700

    scripts: fix gdb install
    Add texinfo requirement
    Change to earlier version of gdb, newer version requires
    libgmp
    
    Signed-off-by: Alexia Ingerson <alexia.ingerson@intel.com>

--- scripts/install_gdb.sh
+++ scripts/install_gdb.sh
@@ -4,11 +4,15 @@ mkdir gdb_build
 cd gdb_build
 
 mkdir gdb
+mkdir texinfo
 
-wget https://ftp.gnu.org/gnu/gdb/gdb-13.2.tar.gz -O gdb.tar.gz
+wget https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.tar.gz -O texinfo.tar.gz
+wget https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.gz -O gdb.tar.gz
 
 tar -xvf gdb.tar.gz -C gdb --strip-components=1
+tar -xvf texinfo .tar.gz -C texinfo --strip-components=1
 
+(cd texinfo $$ configure --prefix=$HOME && make -j && make install)
 (cd gdb && ./configure --prefix=$HOME && make -j && make install)
 
 cd ..
